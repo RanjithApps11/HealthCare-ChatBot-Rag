f858fa85e5bb3b796c573632bf51c1aa68a86cb2
Ranjith <ranjithkumar.apps11@gmail.com>
2026-02-12 07:53:50 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='healthcare_chatbot_rag',
+    version='0.1',
+    author='Mahan',
+    description='A healthcare chatbot using Retrieval-Augmented Generation (RAG) techniques.',
+    author_email='ranjithkumar.apps11@gmail.com',
+    packages=find_packages(),
+    install_requires=[] 
+)
